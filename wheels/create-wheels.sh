15d2d99268da90f6eb2748a1a02f36ddb4c9c950
Maik Riechert <maik.riechert@arcor.de>
2020-08-24 21:31:46 +0100

    move wheels script

--- wheels/create-wheels.sh
+++ wheels/create-wheels.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-rm -rf tmp
+rm -rf *.whl tmp
 mkdir tmp
 cd tmp
 
 # gprof2dot is not yet published as wheel.
 git clone -b 2019.11.30 https://github.com/jrfonseca/gprof2dot.git --depth 1
 cd gprof2dot
-pip3 wheel -w ../../wheels .
+pip3 wheel -w ../.. .
